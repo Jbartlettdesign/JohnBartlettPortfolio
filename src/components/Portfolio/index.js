2a018364ce8dc02474e4125a82c4633acb3950e1
John <jbartlettdesign@gmail.com>
2021-08-06 22:22:40 -0500

    added dingo

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import chatterbox from '../../assets/chatter.png'
 import pegasus from '../../assets/pegasus.png'
-
+import dingo from '../../assets/dingo.png'
 function Portfolio(){
 return(
     <section className = "mainPage">
@@ -16,7 +16,7 @@ return(
          </h2>
             <div className = "portDiv">
                 
-                <img src = {pegasus} alt="tools"/>
+                <img src = {pegasus} alt="pegasus"/>
                 <div className = "links">
                     <a href = "https://jbartlettdesign.github.io/pegasus/"><h4>Pegasus Travel App</h4></a>
                     <a  className = "git" href = "https://github.com/Jbartlettdesign/pegasus">github link</a>
@@ -24,13 +24,20 @@ return(
             </div>
             <div  className = "portDiv">
                 
-                <img src = {chatterbox} alt="tools"/>
+                <img src = {chatterbox} alt="chatterbox"/>
                 <div className = "links">
                     <a href = "https://pure-river-00031.herokuapp.com/"><h4>Chatterbox Social Media Site</h4></a>
                     <a className = "git" href = "https://github.com/Jbartlettdesign/socialMediaSite_ChatterBox">github link</a>
                 </div>
             </div>
-            
+            <div  className = "portDiv">
+                
+                <img className = "dingo" src = {dingo} alt="dingo"/>
+                <div className = "links">
+                    <a href = "https://dingo-dog-walker.herokuapp.com/"><h4>Dingo Dog Walking App</h4></a>
+                    <a className = "git" href = "https://github.com/bc-mern-stack/dingo">github link</a>
+                </div>
+            </div>
          </div>
          </div>
          </div>
